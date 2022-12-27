d9344c38c0b3be3213fda58f7ca1fb50b65a30d3
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2022-12-27 16:21:45 +0300

    feature: weather card component with dates

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import './App.css'
 import { Card } from './components/Card'
 import { useGetWeatherDataQuery } from './components/Card/api/weatherApiSlice'
 function App() {
-
+  const{data,error,isError,isLoading}= useGetWeatherDataQuery({query: "New+York+City"})
+  const name = data && data.city.name
+  const list = data && data.list
+  console.log(list)
   return (
     <div className="w-full h-screen flex justify-center items-center">
+      <Card name={name} weather={list}/>
     </div>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import { useGetWeatherDataQuery } from './api/weatherApiSlice'
 
-export const Card = ({query}) => {
-  const{data,error,isError,isLoading}= useGetWeatherDataQuery({query: query})
+export const Card = ({name, weather}) => {
+  const data = weather.filter((v,i)=>i<=4);
   return (
-    <>
-    </>
+    <div className='flex flex-col items-center justify-center w-1/3 h-2/6 rounded-[50px] border border-black'>
+      <span className='text-3xl font-bold'>
+        {name}
+      </span>
+        {data.map((v,i)=>{
+          return(
+            <div key={i}>
+              {
+                new Date(v.dt * 1000).toLocaleDateString('en-US', {
+                  month: 'long',
+                  day: 'numeric',
+                  year: 'numeric',
+                })
+              }
+            </div>
+          )
+        })}
+    </div>
   )
 }
