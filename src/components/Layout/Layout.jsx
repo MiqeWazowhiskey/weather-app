6955a6da60378eb5ce7f3428c13d95d5759db550
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2022-12-28 02:24:14 +0300

    background

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 export const Layout = ({children}) => {
   return (
-    <div className="max-w-full container bg-[url('./sky.jpg')] bg-no-repeat bg-cover bg-fixed bg-center flex justify-center items-center">
+    <div className="max-w-full container bg-[url('/sky.jpg')] bg-no-repeat bg-cover bg-fixed bg-center flex justify-center items-center">
         {children} 
     </div>
   )
