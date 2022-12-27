e9ef557339084f4a6ccf7538e5425053e4f9d9c9
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2022-12-27 23:57:13 +0300

    hotfix

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Layout = ({children}) => {
   return (
-    <div className='overflow-hidden flex-col w-full h-full flex justify-center items-center' style={{background:"url('./sky.jpg')", backgroundRepeat:"no-repeat", backgroundSize:"100vw 100vh",backgroundAttachment:"fixed"}}>
+    <div className='overflow-hidden flex-col w-full h-full flex justify-center items-center' style={{background:"url('./sky.jpg')", backgroundRepeat:"no-repeat",backgroundAttachment:"fixed"}}>
         {children}
     </div>
   )
