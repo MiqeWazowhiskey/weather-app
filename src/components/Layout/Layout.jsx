5a9908188ed2af2dfa45686114b8837d5472277a
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2022-12-28 00:46:05 +0300

    responsive fix (except galaxy flip)

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import css from './index.module.css'
 export const Layout = ({children}) => {
   return (
-    <div className='overflow-hidden flex-col w-full h-full flex justify-center items-center' style={{background:"url('./sky.jpg')", backgroundRepeat:"no-repeat",backgroundAttachment:"fixed",backgroundSize:'100% 100%'}}>
-        {children}
+    <div className={css.container}>
+        {children} 
     </div>
   )
 }
