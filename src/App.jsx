e421d5df507fecc649d6cf608ceafbe6320f68ee
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2022-12-27 14:12:21 +0300

    fix: fetch data with query prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
 import './App.css'
+import { Card } from './components/Card'
 import { useGetWeatherDataQuery } from './components/Card/api/weatherApiSlice'
 function App() {
 
   return (
-    <div className="App">
-      
+    <div className="w-full h-screen flex justify-center items-center">
     </div>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { useGetWeatherDataQuery } from './api/weatherApiSlice'
+
 export const Card = ({query}) => {
+  const{data,error,isError,isLoading}= useGetWeatherDataQuery({query: query})
   return (
     <>
-
     </>
   )
 }
