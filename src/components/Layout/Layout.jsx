deaa44b7fc3aa25734b11739d3bf2b074fe7d032
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2022-12-28 02:41:03 +0300

    refactor: bg

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 export const Layout = ({children}) => {
   return (
-    <div className="max-w-full container bg-[url('/sky.jpg')] bg-no-repeat bg-cover bg-fixed bg-center flex justify-center items-center">
-        {children} 
-    </div>
+    <>
+      <img className='z-[-10] fixed top-0 left-0 object-fit w-[100vw] h-[100vh]' src='/sky.jpg'/>
+      <div className="z-20 max-w-full container bg-no-repeat bg-cover bg-fixed bg-center flex justify-center items-center">
+          {children} 
+      </div>
+    </>
   )
 }
