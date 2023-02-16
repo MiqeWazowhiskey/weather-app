6d8ab9f104eb87f0ae9fdc09053793c2a165140e
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2023-02-16 17:54:54 +0300

    refactor: new ui

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,39 +1,34 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { useGetWeatherDataQuery } from './api/weatherApiSlice'
-import {TiWeatherSunny as Sunny} from 'react-icons/ti'
 export const Card = ({query}) => {
   const{data,error,isError,isSuccess}= useGetWeatherDataQuery({query: query})  
   return (
-    <>
     
-      <div className='flex flex-col whitespace-nowrap border items-center w-5/6 lg:w-1/2 h-1/2 rounded-[50px] mb-12 p-5 bg-white bg-opacity-50'>
-        <h1 className='text-3xl font-bold p-5 text-[#3D4B5F] '>
-            {isSuccess && data.city.name}
-        </h1>
+      <div className='flex flex-wrap whitespace-nowrap border items-center justify-center w-full rounded-[50px] mb-12 p-5 bg-white bg-opacity-50'>
+        <div className='items-center justify-center w-full'>
+          <h1 className='text-3xl w-full text-center font-bold p-2 text-[#3D4B5F]'>
+              {isSuccess && data.city.name}
+          </h1>
 
-        <div className='w-1/2 flex flex-col items-center'>
-          <p className=''>
-            {isSuccess && data.list[1].weather[0].main}
-          </p>
-          <p>
-            {isSuccess && data.list[1].weather[0].description}
-          </p>
-          <p>
-          {isSuccess && data.list[1].dt_txt.substring(0,10)}
-          </p>
-          <p>
-          {isSuccess && data.list[1].dt_txt.substring(10,16)}
-          </p>
-          {isSuccess && <img className='bg-[#8EC1D6] rounded-full bg-opacity-40' width={82} src={`http://openweathermap.org/img/wn/${data.list[1].weather[0].icon}.png`}/>}
+          <div className='flex w-full text-2xl justify-center flex-col gap-x-10 items-center'>
+            <p>
+              {isSuccess && data.list[0].weather[0].description.toUpperCase()}
+            </p>
+            <p>
+            {isSuccess && data.list[0].dt_txt.substring(10,16)}
+            </p>
+            <p>
+            {isSuccess && (data.list[0].main.temp-273.15).toString().substring(0,4).length==4 
+              ? (data.list[0].main.temp-273.15).toString().substring(0,4) 
+              : (data.list[0].main.temp-273.15).toString().substring(0,4).concat(".00")} °C
+            </p>
 
+          </div>
         </div>
-   
-      </div>
-      <div className='flex flex-wrap gap-y-10 gap-x-5 items-center justify-center w-4/6 '>
           {isSuccess && data.list.map((v,i)=>{
             return(
-              <div key={i} className="flex flex-col items-center lg:gap-y-10 rounded-[50px] bg-[#8EC1D6] bg-opacity-40 p-5 shadow-xl">
-                <div className='lg:w-[96px] w-[196px] whitespace-pre-line text-center flex p-2'>
+              <div key={i} className="flex flex-row items-center rounded-[50px] m-2 bg-[#8EC1D6] bg-opacity-40 shadow-xl">
+                <div className='w-5/6 whitespace-nowrap text-center flex p-2 text-xs'>
                   {
                     new Date(v.dt * 1000).toLocaleDateString('en-US', {
                       month: 'long',
@@ -45,13 +40,16 @@ export const Card = ({query}) => {
                   }
                 </div>
                 <span>
-                  {(v.main.temp-273.15).toString().substring(0,4)} °C
+                  {(v.main.temp-273.15).toString().substring(0,4).length==4 
+                  ? (v.main.temp-273.15).toString().substring(0,4)
+                  : (v.main.temp-273.15).toString().substring(0,4).concat(".00")} °C
                 </span>
-                <img className='bg-[#8EC1D6] rounded-full bg-opacity-40' width={48} src={`http://openweathermap.org/img/wn/${v.weather[0].icon}.png`}/>
+                <img className='bg-[#8EC1D6] rounded-full bg-opacity-40 ml-3' 
+                     width={48} 
+                     src={`http://openweathermap.org/img/wn/${v.weather[0].icon}.png`}/>
             </div>
             )
           })}
       </div>
-    </>
-  )
+    )
 }
