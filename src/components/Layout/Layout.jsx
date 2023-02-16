78c99faf1b54b0e5e82757958f08603aadab40a1
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2023-02-16 17:17:05 +0300

    refactor: remove bg image

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 export const Layout = ({children}) => {
   return (
     <>
-      <img className='z-[-10] fixed top-0 left-0 object-fit w-[100vw] h-[100vh]' src='/sky.jpg'/>
-      <div className="z-20 max-w-full container bg-no-repeat bg-cover bg-fixed bg-center flex justify-center items-center">
+      <div className="bg-gradient-to-b from-[#BBE6E4] to-[#053760] z-20 max-w-full container bg-no-repeat bg-cover bg-fixed bg-center flex justify-center items-center">
           {children} 
       </div>
     </>
